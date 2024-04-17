1f78de57575c0d8fab5ee49d024775e61608b3b0
Christian <web.dog04@gmail.com>
2024-04-17 09:26:05 -0500

    adding MobileNav

--- src/components/shared/MobileNav.tsx
+++ src/components/shared/MobileNav.tsx
@@ -1,10 +1,44 @@
-import { SignedIn, UserButton } from "@clerk/nextjs"
+"use client"
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
 import Image from "next/image"
 import Link from "next/link"
 
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
+import { bottomLinks, navLinks } from "@/constants"
+import { usePathname } from "next/navigation"
+import { Button } from "../ui/button"
 
 const MobileNav = () => {
+  const pathName = usePathname()
+
+  interface NavLink {
+    label: string
+    route: string
+    icon: string
+  }
+  const createList = (linkList: NavLink[]) => {
+    return (
+      <>
+        {linkList.map((link) => {
+          const isActive = link.route === pathName
+          return (
+            <li
+              key={link.route}
+              className={`${
+                isActive && "gradient-test"
+              } p-18 flex whitespace-nowrap text-dark-700`}
+            >
+              <Link className="sidebar-link cursor-pointer" href={link.route}>
+                <Image src={link.icon} alt="logo" width={24} height={24} />
+                {link.label}
+              </Link>
+            </li>
+          )
+        })}
+      </>
+    )
+  }
+
   return (
     <header className="header">
       <Link href="/" className="flex items-center gap-2 md:py-2">
@@ -37,10 +71,21 @@ const MobileNav = () => {
                   width={152}
                   height={23}
                 />
+
+                <ul className="header-nav_elements">
+                  {createList(navLinks)}
+                  {createList(bottomLinks)}
+                </ul>
               </>
             </SheetContent>
           </Sheet>
         </SignedIn>
+
+        <SignedOut>
+          <Button asChild className="button bg-purple-gradient bg-cover">
+            <Link href="/sign-in">Login</Link>
+          </Button>
+        </SignedOut>
       </nav>
     </header>
   )
