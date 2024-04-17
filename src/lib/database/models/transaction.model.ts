248b6e6c8df9ea33b76164a53066fd921cadb25f
Christian <web.dog04@gmail.com>
2024-04-17 14:50:47 -0500

    adding mongoDB

---
+++ src/lib/database/models/transaction.model.ts
@@ -0,0 +1,15 @@
+import { Schema, model, models } from "mongoose"
+
+const TransactionSchema = new Schema({
+  createdAt: { type: Date, default: Date.now },
+  stripeId: { type: String, required: true, unique: true },
+  amount: { type: Number, required: true },
+  credits: { type: Number },
+  plan: { type: String },
+  buyer: { type: Schema.Types.ObjectId, ref: "User" },
+})
+
+const Transaction =
+  models?.Transaction || model("Transaction", TransactionSchema)
+
+export default Transaction
